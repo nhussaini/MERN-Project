4008ec515b67032e9703146589329cdba3350ce0
nhussaini <nasrullah_hussaini@yahoo.com>
2021-10-21 17:26:51 -0400

    add navlinks

--- FRONTEND/src/shared/components/Navigation/MainNavigation.jsx
+++ FRONTEND/src/shared/components/Navigation/MainNavigation.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 import MainHeader from './MainHeader';
+import NavLinks from './NavLinks';
 
 import './MainNavigation.css';
 
@@ -16,7 +18,9 @@ const MainNavigation = (props) => {
       <h1 className="main-navigation__title">
         <Link to="/">YourPlaces</Link>
       </h1>
-      <nav>...</nav>
+      <nav>
+        <NavLinks />
+      </nav>
     </MainHeader>
   );
 };
