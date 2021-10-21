3b94608b6a0692a192a0a6bc7e62b30a1d352d39
nhussaini <nasrullah_hussaini@yahoo.com>
2021-10-21 12:17:27 -0400

    create UserList and add a static user

--- FRONTEND/src/user/pages/Users.jsx
+++ FRONTEND/src/user/pages/Users.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Nasrullah',
+      image:
+        'https://nasrullah-hussaini.web.app/static/media/myPic.64d1d2f4.jpg',
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
